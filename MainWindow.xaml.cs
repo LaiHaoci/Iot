89ecb14e92083ca45a10d67aebee314b83122527
LaiHaoci <hclai0527@gmail.com>
2025-10-19 17:16:24 +0800

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using IotDeviceManager.ViewModels;
+
+namespace IotDeviceManager
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+        private void OpenAddDeviceWindow(object sender, RoutedEventArgs e)
+        {
+            var addWindow = new AddDeviceWindow(); 
+            addWindow.Owner = this;
+            addWindow.ShowDialog(); 
+        }
+
+        private void OpenLogPage(object sender, RoutedEventArgs e)
+        {
+            LogPage logWindow = new LogPage();
+            logWindow.Owner = this;
+            logWindow.ShowDialog();
+        }
+    }
+}
---
+++ RelayCommands.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace IotDeviceManager
+{
+    public class RelayCommands : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public RelayCommands(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            var handler = CanExecuteChanged;
+            if (handler != null) handler(this, EventArgs.Empty);
+        }
+    }
+}
